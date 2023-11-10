6d854a36f2ed8094f8b061d0bc1e455e3ec3708f
Rio McCoy <s0323792@student.rockvalleycollege.edu>
2023-11-09 20:20:48 -0600

    Initial Commit

---
+++ SelfHelpApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SelfHelpApp
+//
+//  Created by Rio D. McCoy on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfHelpApp/SelfHelpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfHelpAppApp.swift
+//  SelfHelpApp
+//
+//  Created by Rio D. McCoy on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SelfHelpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
